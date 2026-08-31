1b50868112ffe244d6c2de77b81e8a109e62f384
Success <okpuniyi@gmail.com>
2026-08-31 08:21:50 +0100

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,12 +1,3 @@
-print("QUESTION 2A")
-print("Characteristics of a Good Numerical Algorithm")
-print("1. Accuracy")
-print("2. Efficiency")
-print("3. Stability")
-print("4. Convergence")
-print("5. Reliability")
-print("6. Simplicity")
-
 
 print("\nQUESTION 2B")
 print("Elimination Method")
@@ -26,3 +17,31 @@ z = 1 / 5
 print("x =", x)
 print("y =", y)
 print("z =", z)
+
+
+# Question 2(c) - Bisection Method
+
+def f(x):
+    return x**3 - 4
+
+a = 1
+b = 2
+
+print("Question 2(c)")
+print("Iteration    a        b        c")
+
+for i in range(1, 6):
+
+    c = (a + b) / 2
+
+    print(i, "       ",
+          round(a, 5),
+          round(b, 5),
+          round(c, 5))
+
+    if f(a) * f(c) < 0:
+        b = c
+    else:
+        a = c
+
+print("\nApproximate root =", round(c, 5))
